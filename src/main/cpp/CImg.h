163fb17d4b29a0297ad25fc92b3cc49302f7134c
David Tschumperlé <david.tschumperle@gmail.com>
2018-06-21 16:56:44 +0200

    Auto-commit for release 2.3.1_pre

--- src/main/cpp/CImg.h
+++ src/main/cpp/CImg.h
@@ -54,7 +54,7 @@
 
 // Set version number of the library.
 #ifndef cimg_version
-#define cimg_version 230
+#define cimg_version 231
 
 /*-----------------------------------------------------------
  #
