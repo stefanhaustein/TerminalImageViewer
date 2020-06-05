935c041ad254aae1d4d2eea1b94749b00797ad41
Austin Atchley <austinatchley@gmail.com>
2020-06-04 19:39:10 -0500

    Fix std::array initialization error

--- src/main/cpp/tiv.cpp
+++ src/main/cpp/tiv.cpp
@@ -122,8 +122,8 @@ const unsigned int BITMAPS[] = {
 
 
 struct CharData {
-  std::array<int, 3> fgColor = {0};
-  std::array<int, 3> bgColor = {0};
+  std::array<int, 3> fgColor = std::array<int, 3>{0, 0, 0};
+  std::array<int, 3> bgColor = std::array<int, 3>{0, 0, 0};
   int codePoint;
 };
 
